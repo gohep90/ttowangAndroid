fac90080302888a45facd3076d6d99db4ff7b01f
Park <gohep90>
2016-08-02 18:37:14 +0900

    지도구현

--- app/src/main/java/com/app/ttowang/ttowang/Main/Business/business.java
+++ app/src/main/java/com/app/ttowang/ttowang/Main/Business/business.java
@@ -1,12 +1,53 @@
 package com.app.ttowang.ttowang.Main.Business;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.app.ttowang.ttowang.R;
 
-public class business extends AppCompatActivity {
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class business extends AppCompatActivity implements OnMapReadyCallback {
+
+    static final LatLng SEOUL = new LatLng(37.56, 126.97);
+    private GoogleMap googleMap;
+
+    @Override
+    public void onMapReady(final GoogleMap map) {
+        googleMap = map;
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+        Marker seoul = googleMap.addMarker(new MarkerOptions().position(SEOUL)
+                .title("Seoul"));
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom( SEOUL, 15));
+
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +56,11 @@ public class business extends AppCompatActivity {
         setContentView(R.layout.business);
 
         initToolbar();
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
 
     private void initToolbar() {
