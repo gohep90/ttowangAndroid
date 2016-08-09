989f88e2c56bf11f258caad84bf4564deafbd1c3
Lee-jae-hoon2 <dkrakghs1@naver.com>
2016-08-09 17:23:58 +0900

    설정 uI함 재훈

--- app/src/main/java/com/app/ttowang/ttowang/Main/Setting/Mainsetting.java
+++ app/src/main/java/com/app/ttowang/ttowang/Main/Setting/Mainsetting.java
@@ -2,14 +2,17 @@ package com.app.ttowang.ttowang.Main.Setting;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.TextViewCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.app.ttowang.ttowang.Main.MainActivity;
@@ -22,7 +25,14 @@ public class Mainsetting extends Fragment {
 
     View view;
     Button modeChange;
+    Button myInfo;
     Context context;
+
+    TextView myName, myPhone, myEmail;
+
+
+    public static SharedPreferences sharedPreferences;
+    public static SharedPreferences.Editor Edit;
 
     public static Mainsetting createInstance(int itemsCount) {
         Mainsetting Setting = new Mainsetting();
@@ -38,6 +48,31 @@ public class Mainsetting extends Fragment {
 
         view = inflater.inflate(R.layout.setting,container, false);
         modeChange = (Button) view.findViewById(R.id.modeChange);
+        myInfo=(Button) view.findViewById(R.id.myInfo);//위에꺼처럼 myInfo해
+
+        myName=(TextView)view.findViewById(R.id.myName);//xml에서 TextView id가져와
+        myPhone=(TextView)view.findViewById(R.id.myPhone);//xml에서 TextView id가져와
+        myEmail=(TextView)view.findViewById(R.id.myEmail);//xml에서 TextView id가져와
+
+        sharedPreferences = getActivity().getSharedPreferences("setting",getActivity().MODE_PRIVATE);//초기화
+        Edit = sharedPreferences.edit();
+
+        Edit.putString("name", "이재훈");//데이터 저장하기
+        Edit.putString("phone","01031933763");
+        Edit.putString("email","hoon123@naver.com");
+        Edit.commit();//커밋
+
+
+
+
+        String myname = sharedPreferences.getString("name", "" );//값 가져와서 String에 저장
+        String Phone = sharedPreferences.getString("phone", "" );
+        String Email = sharedPreferences.getString("email", "" );
+        myName.setText(myname);//가져온 String myName에다가 넣어
+        myPhone.setText(Phone);
+        myEmail.setText(Email);
+
+
 
         modeChange.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -50,7 +85,21 @@ public class Mainsetting extends Fragment {
                 getActivity().startActivityForResult(intent, 0);
             }
         });
-        return view;
+
+
+        myInfo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), myInfoEdit.class);//ChangeModeMain수정
+                startActivity(intent);
+            }
+        });
+
+
+        //myinfo.~새창열리게해
+        //Intent intent = new Intent(getContext(),business.class);
+        //startActivity(intent);
+       return view;
     }
 
 }
---
+++ app/src/main/java/com/app/ttowang/ttowang/Main/Setting/myInfoEdit.java
@@ -0,0 +1,16 @@
+package com.app.ttowang.ttowang.Main.Setting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.ttowang.ttowang.R;
+
+
+public class myInfoEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_info_edit);
+    }
+}
