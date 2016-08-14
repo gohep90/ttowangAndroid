b9d0a1d447e48e90e9cc8a0c8cb616bb6e567648
skillist <srpgs2@gmail.com>
2016-08-14 16:01:24 +0900

    재훈이가 내 정보 수정 한거

--- app/src/main/java/com/app/ttowang/ttowang/Main/Setting/Mainsetting.java
+++ app/src/main/java/com/app/ttowang/ttowang/Main/Setting/Mainsetting.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.TextViewCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +17,6 @@ import android.widget.Toast;
 import com.app.ttowang.ttowang.Main.MainActivity;
 import com.app.ttowang.ttowang.ModeChange.ChangeModeMain;
 import com.app.ttowang.ttowang.R;
-import com.app.ttowang.ttowang.Main.Setting.myInfoEdit;
 
 public class Mainsetting extends Fragment {
     public final static String ITEMS_COUNT_KEY = "home$ItemsCount";
@@ -30,9 +28,9 @@ public class Mainsetting extends Fragment {
 
     TextView myName, myPhone, myEmail;
 
+    public static SharedPreferences sharedPreferences;//이거랑
+    public static SharedPreferences.Editor Edit;//이거수정하는거 2개가필요
 
-    public static SharedPreferences sharedPreferences;
-    public static SharedPreferences.Editor Edit;
 
     public static Mainsetting createInstance(int itemsCount) {
         Mainsetting Setting = new Mainsetting();
@@ -46,7 +44,6 @@ public class Mainsetting extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        context = MainActivity.mContext;
         view = inflater.inflate(R.layout.setting,container, false);
         modeChange = (Button) view.findViewById(R.id.modeChange);
         myInfo=(Button) view.findViewById(R.id.myInfo);//위에꺼처럼 myInfo해
@@ -55,22 +52,24 @@ public class Mainsetting extends Fragment {
         myPhone=(TextView)view.findViewById(R.id.myPhone);//xml에서 TextView id가져와
         myEmail=(TextView)view.findViewById(R.id.myEmail);//xml에서 TextView id가져와
 
-        sharedPreferences = getActivity().getSharedPreferences("setting",getActivity().MODE_PRIVATE);//초기화
-        Edit = sharedPreferences.edit();
+        sharedPreferences = getActivity().getSharedPreferences("setting",getActivity().MODE_PRIVATE);//그냥 setting이라는 이름의 객체를 얻어서 초기화
+        Edit = sharedPreferences.edit();//수정하겠다는 객체 얻기
 
-        Edit.putString("name", "이재훈");//데이터 저장하기
-        Edit.putString("phone","01031933763");
-        Edit.putString("email","hoon123@naver.com");
-        Edit.commit();//커밋
+        if(sharedPreferences.getString("name", "").equals("")) {//저장된게 없으면 실행하고 저장된게 있으면 하지마 근데 이거 없어도 됨 뭐지?
+            Edit.putString("name", "이재훈");//이 함수를 이용해서 name에 데이터 저장하기
+            Edit.putString("phone", "01031933763");//내정보로 수정해도 앱 다시킬때 이 코드 수행되니 안변해야 정상 아닌가?
+            Edit.putString("email", "hoon123@naver.com");
+            Edit.commit();//커밋
+        }
 
 
 
 
-        String myname = sharedPreferences.getString("name", "" );//값 가져와서 String에 저장
-        String Phone = sharedPreferences.getString("phone", "" );
-        String Email = sharedPreferences.getString("email", "" );
+        String myname = sharedPreferences.getString("name", "이름" );//수정말고 원본객체에서 name이름의 문자열을 얻어와
+        String Phone = sharedPreferences.getString("phone", "폰" );
+        String Email = sharedPreferences.getString("email", "메일" );
         myName.setText(myname);//가져온 String myName에다가 넣어
-        myPhone.setText(Phone);
+        myPhone.setText(Phone);//맨위에 선언한 textview에 String넣어
         myEmail.setText(Email);
 
 
@@ -89,19 +88,14 @@ public class Mainsetting extends Fragment {
 
 
         myInfo.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, myInfoEdit.class);//ChangeModeMain수정
+            public void onClick(View v){
+                Intent intent = new Intent(getContext(),myInfoEdit.class);
                 startActivity(intent);
-
             }
         });
 
 
-        //myinfo.~새창열리게해
-        //Intent intent = new Intent(getContext(),business.class);
-        //startActivity(intent);
-       return view;
+        return view;
     }
 
 }
--- app/src/main/java/com/app/ttowang/ttowang/Main/Setting/myInfoEdit.java
+++ app/src/main/java/com/app/ttowang/ttowang/Main/Setting/myInfoEdit.java
@@ -1,16 +1,58 @@
 package com.app.ttowang.ttowang.Main.Setting;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.ttowang.ttowang.R;
 
 
 public class myInfoEdit extends AppCompatActivity {
+    //EditText 여러개 선언
+    TextView text_tel, edt_name, edt_year, edt_month, edt_day, edt_email;
+    Button btn_edit;
 
+    //다른곳에 선언되어있는 SharedPreferences 가져오기
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_info_edit);
+        setContentView(R.layout.myinfoedit);//내정보수정 창 띄워주고
+        btn_edit=(Button)findViewById(R.id.btn_edit);
+        edt_name=(TextView)findViewById(R.id.edt_name);//(TextView)findViewById(R.id.~);
+        text_tel=(TextView)findViewById(R.id.text_tel);
+        edt_email=(TextView)findViewById(R.id.edt_email);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("setting",MODE_PRIVATE);//쉐어드객체얻기
+
+        //String name = sharedPreferences.getString("name", "");
+        edt_name.setText(sharedPreferences.getString("name", ""));
+        text_tel.setText(sharedPreferences.getString("phone", ""));
+        edt_email.setText(sharedPreferences.getString("email",""));
+        //폰에 들어있는 내정보를 edittext로 뿌려줘
+
+        btn_edit.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                SharedPreferences sharedPreferences = getSharedPreferences("setting",MODE_PRIVATE);//쉐어드객체 다시 얻기
+                SharedPreferences.Editor Edit= sharedPreferences.edit();
+
+
+                Edit.putString("name", edt_name.getText().toString());//이 함수를 이용해서 name에 데이터 저장하기,
+                Edit.putString("phone", text_tel.getText().toString());
+                Edit.putString("email", edt_email.getText().toString());
+                Edit.commit();
+
+                Toast.makeText(myInfoEdit.this, edt_name.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                finish();
+                //Intent intent = new Intent(getApplicationContext(),Mainsetting.class);
+                //startActivity(intent);
+            }
+        });
+        //저장버튼이있어서 저장버튼 누르면 버튼 메소드안에서 내용을 바꿔줘
+        //그러면 setting에서 새로고침하면 바뀌겠지
     }
 }
