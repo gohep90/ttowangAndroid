a16d851a86fbfe4e6d6e7f59db14f739b2148dd6
skillist <srpgs2@gmail.com>
2016-08-31 12:57:27 +0900

    비즈니스 사진 하나 추가해서 서버 전송 했음

--- app/src/main/java/com/app/ttowang/ttowang/ModeChange/MyShop/myBusinessShop/myBusinessStoreAdd.java
+++ app/src/main/java/com/app/ttowang/ttowang/ModeChange/MyShop/myBusinessShop/myBusinessStoreAdd.java
@@ -5,16 +5,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.app.ttowang.ttowang.Main.MainActivity;
@@ -27,8 +33,12 @@ import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Enumeration;
@@ -62,15 +72,16 @@ public class myBusinessStoreAdd extends AppCompatActivity {
             businessMenu,
             businessBenefit,
             businessGroup,
-            photo1Uri="",
-            photo2Uri="",
-            photo3Uri="",
-            photo4Uri="";
+            photo1Path="",
+            photo2Path="",
+            photo3Path="",
+            photo4Path="";
 
     String userId;
 
     Button businessphoto1;
     ImageView photo1;
+    Uri photouri1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,9 +144,11 @@ public class myBusinessStoreAdd extends AppCompatActivity {
                     !businessAddress.equals("") &
                     !businessMenu.equals("") &
                     !businessBenefit.equals("") &
-                    !businessGroup.equals("")
+                    !businessGroup.equals("") &
+                    !photo1Path.equals("")
                 ){
-                    new businessAddStoreAsyncTask().execute();
+                    //new businessAddStoreAsyncTask().execute();
+                    new ImageAsyncTask().execute();
                     //Intent intent = new Intent(getApplicationContext(), myBusinessCouponAdd.class);   //인텐트로 넘겨줄건데요~
                     //intent.putExtra("currentViewPager", currentViewPager);
                     //startActivity(intent);
@@ -163,27 +176,146 @@ public class myBusinessStoreAdd extends AppCompatActivity {
             case 0:
                 if(resultCode== Activity.RESULT_OK) {
                     try {
-                        photo1Uri = String.valueOf(data.getData());
-                        businessphoto1.setText(photo1Uri);
+                        photouri1 = data.getData();
 
-                        Log.i("매장 추가 - ", "가져온 이미지 " + photo1Uri);
 
-                        Bitmap bm = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.parse(photo1Uri));
+                        Log.i("매장 추가 - ", "가져온 이미지 " + photouri1);
+
+                        Bitmap bm = MediaStore.Images.Media.getBitmap(getContentResolver(), photouri1);
+
+                        //절대 경로 가져오기
+                        String wholeID = DocumentsContract.getDocumentId(photouri1);
+                        // Split at colon, use second item in the array
+                        String id = wholeID.split(":")[1];
+                        String[] column = { MediaStore.Images.Media.DATA };
+                        // where id is equal to
+                        String sel = MediaStore.Images.Media._ID + "=?";
+                        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                        column, sel, new String[]{ id }, null);
+                        int columnIndex = cursor.getColumnIndex(column[0]);
+                        if (cursor.moveToFirst()) {
+                            photo1Path = cursor.getString(columnIndex);
+                        }
+                        cursor.close();
+                        Log.i("매장 추가 - ", "가져온 이미지 절대경로" + photo1Path);
+
+                        businessphoto1.setText(photo1Path);
+                        /*
+                        String[] proj = { MediaStore.Images.Media.DATA };
+
+                        CursorLoader cursorLoader = new CursorLoader(this, photouri1, proj, null, null, null);
+                        Cursor cursor = cursorLoader.loadInBackground();
+
+                        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                        cursor.moveToFirst();
+
+                        Log.i("매장 추가 - ", "가져온 이미지 절대경로" + cursor.getString(column_index));
+                        */
+/*
+                        String[] proj = {MediaStore.Images.Media.DATA};
+                        Cursor cursor = getContentResolver().query(photouri1.getPath(), proj, null, null, null);
+                        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+                        cursor.moveToFirst();
+
+                        String imgPath = cursor.getString(column_index);
+                        String photoName = imgPath.substring(imgPath.lastIndexOf("/") + 1);
+                        Log.i("매장 추가 - ", "가져온 이미지 절대경로" + imgPath);
+*/
+ /*                       String imagePath = uri.getPath();
+                        Log.i("매장 추가 - ", "가져온 이미지 절대경로" + imagePath);
+*/
+                        //이미지 파일의 절대 경로 받아오기.
+                        /*
+                        String[] projection = { MediaStore.Images.Media.DATA };
+                        Cursor cursor = managedQuery(uri, projection, null, null, null);
+                        int column_index = cursor
+                                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                        cursor.moveToFirst();
+*/
+                        //String absolutePath = cursor.getString(column_index);
+                        //String absolutePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+                        //Log.i("매장 추가 - ", "가져온 이미지 절대경로" + absolutePath);
+                        /*
+                        String[] projection = { MediaStore.Images.Media.DATA };
+                        Cursor c = getContentResolver().query(uri, projection,null,null,null);
+                        c.moveToFirst();
+                        String absolutePath = c.getString(c.getColumnIndex(MediaStore.MediaColumns.DATA));
+                        Log.i("매장 추가 - ", "가져온 이미지 절대경로" + absolutePath);
+*/
+
+/*
+                        String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                        Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+                        cursor.moveToFirst();
+
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        String picturePath = cursor.getString(columnIndex);
+                        cursor.close();
+*/
+                        /*
+                        //Exif에서 회전 정보 가져오기
+                        ExifInterface exif = new ExifInterface(imagePath);
+                        int exifOrientation = exif.getAttributeInt(
+                                ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+                        Log.i("매장 추가 - ","사진 회전 정보 : " + exifOrientation);
+
+                        int exifDegree;
+                        if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_90)
+                        {
+                            exifDegree = 90;
+                            Log.i("매장 추가 - ","사진 회전 각도 : " + exifDegree);
+                        }else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_180)
+                        {
+                            exifDegree = 180;
+                            Log.i("매장 추가 - ","사진 회전 각도 : " + exifDegree);
+                        }else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_270)
+                        {
+                            exifDegree = 270;
+                            Log.i("매장 추가 - ","사진 회전 각도 : " + exifDegree);
+                        }else{
+                            exifDegree = 0;
+                            Log.i("매장 추가 - ","사진 회전 각도 : " + exifDegree);
+                        }
+                        Log.i("매장 추가 - ","사진 회전 각도 : " + exifDegree);
+
+
+                        //가져온 정보로 이미지 회전하기
+
+                        Matrix m = new Matrix();
+                        m.setRotate(exifDegree, (float) bm.getWidth() / 2,
+                                (float) bm.getHeight() / 2);
+
+                        try
+                        {
+                            Bitmap converted = Bitmap.createBitmap(bm, 0, 0,
+                                    bm.getWidth(), bm.getHeight(), m, true);
+                            if(bm != converted)
+                            {
+                                bm.recycle();
+                                bm = converted;
+                            }
+                        }
+                        catch(OutOfMemoryError ex)
+                        {
+                            // 메모리가 부족하여 회전을 시키지 못할 경우 그냥 원본을 반환합니다.
+                        }
+*/
                         //photo1.setImageBitmap(bm);
-                        Bitmap reSized = Bitmap.createScaledBitmap(bm, 2000,2000, true);
+                        int height = bm.getHeight();
+                        int width = bm.getWidth();
+
+                        Bitmap reSized = Bitmap.createScaledBitmap(bm, (width * 1000) / height,1000, true);
                         photo1.setImageBitmap(reSized); //이미지 뷰에 리사이즈 된 비트맵을 넣는다.
 
 
-                        //이미지 파일의 절대 경로 받아오기
-
-                        Cursor c = getContentResolver().query(Uri.parse(photo1Uri), null,null,null,null);
-                        c.moveToNext();
-                        String absolutePath = c.getString(c.getColumnIndex(MediaStore.MediaColumns.DATA));
-                        Log.i("매장 추가 - ", "가져온 이미지 절대경로" + absolutePath);
 
 
-                    } catch (Exception e) {
 
+                    } catch (IOException e){
+                        Log.i("매장 추가 - " , String.valueOf(e));
                     }
                 }
         }
@@ -318,6 +450,173 @@ public class myBusinessStoreAdd extends AppCompatActivity {
 
             }catch(JSONException e){
                 e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+    public class ImageAsyncTask extends AsyncTask<String,Integer,String> {
+        @Override
+        protected String doInBackground(String... params) {  // 통신을 위한 Thread
+
+                doFileUpload();
+
+            return null;
+        }
+
+        public String doFileUpload() {
+            URL connectUrl = null;
+            HttpURLConnection conn = null;
+            DataOutputStream dos = null;
+
+            try {
+                String boundary = "^******^";
+
+                // 데이터 경계선
+                String delimiter = "\r\n--" + boundary + "\r\n";
+
+                Log.i("이미지 :",  photo1Path);
+                FileInputStream fileInputStream = new FileInputStream(photo1Path);
+                Log.i("이미지추가 ",  "했음");
+                connectUrl = new URL("http://" + MainActivity.ip + ":8080/ttowang/businessAdd.do");
+
+                // open connection
+                conn = (HttpURLConnection) connectUrl.openConnection();
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+                Log.i("이미지 :", "연결??");
+                // write data
+                dos = new DataOutputStream(conn.getOutputStream());
+
+
+                StringBuffer pd = new StringBuffer();
+                pd.append(delimiter);
+                pd.append(setText("userId", String.valueOf(userId)));
+                pd.append(delimiter);
+                pd.append(setText("businessLicense", businessLicense));
+                pd.append(delimiter);
+                pd.append(setText("businessName", businessName));
+                pd.append(delimiter);
+                pd.append(setText("businessTel", businessTel));
+                pd.append(delimiter);
+                pd.append(setText("businessInfo", businessInfo));
+                pd.append(delimiter);
+                pd.append(setText("businessTime", businessTime));
+                pd.append(delimiter);
+                pd.append(setText("businessAddress", businessAddress));
+                pd.append(delimiter);
+                pd.append(setText("businessMenu", businessMenu));
+                pd.append(delimiter);
+                pd.append(setText("businessBenefit", businessBenefit));
+                pd.append(delimiter);
+                pd.append(setText("businessLat",""));
+                pd.append(delimiter);
+                pd.append(setText("businessLng",""));
+                pd.append(delimiter);
+                pd.append(setText("businessType", "1"));
+                pd.append(delimiter);
+                pd.append(setText("businessGroup", businessGroup));
+                pd.append(delimiter);
+                pd.append(setImage("image", photo1Path));
+                pd.append("\r\n");
+
+                dos.writeUTF(pd.toString());
+
+                int bytesAvailable = fileInputStream.available();
+                int maxBufferSize = 1024;
+                int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+
+                byte[] buffer = new byte[bufferSize];
+                int bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0)
+                {
+                    dos.write(buffer, 0, bufferSize); //이미지 전송
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                }
+
+                dos.writeBytes(delimiter);
+
+                //이미지와 텍스트값을 전송할때와 다르게 뒤에 --가 더 붙으며 전송데이터 값의 끝
+                fileInputStream.close();
+                dos.flush(); //전송완료
+
+                //전송을 완료하고 꼭 서버에서 전송된 결과값을 받아야 완벽하게 업로드된 텍스트
+                //이미지가 저장된다. 중요하다 ..
+                BufferedReader rd = null;
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                String line = null;
+                String result = "";
+                while ((line = rd.readLine()) != null) {
+                    result += line;
+                }
+                dos.close();
+                return result;
+            } catch (MalformedURLException e) {
+                Log.d("MalformedURLException", e.toString());
+            }catch(Exception ex){
+                Log.d("MalformedURLException", ex.toString());
+            }
+
+            return null;
+        }
+
+        //텍스트값 처리
+        private String setText(String tname, String text){
+            return "Content-Disposition: form-data; name=\""+tname+"\"r\n\r\n"+text;
+        }
+
+        //이미지값 처리
+        private String setImage(String iname, String image){
+            return "Content-Disposition: form-data; name=\""+iname+"\";filename=\""+image+"\"\r\n";
+        }
+
+        protected void onPostExecute(String result){  //Thread 이후 UI 처리 result는 Thread의 리턴값!!!
+            Log.i("myBusinessShop - ", " 내 매장 등록");
+
+            if(result!=null) {
+                Log.i("서버에서 받은 전체 내용 : ", result);
+                try {
+
+                    JSONObject json = new JSONObject(result);
+                    JSONArray jArr = json.getJSONArray("List");
+                    json = jArr.getJSONObject(0);
+                    Log.i("서버에서 받은 비즈니스 아이디 : ", json.getString("businessId"));
+
+                    myBusinessShop.adapter.addItem(
+                            "1",
+                            json.getString("businessId"),
+                            businessLicense,
+                            businessName,
+                            businessTel,
+                            businessInfo,
+                            businessTime,
+                            businessAddress,
+                            businessMenu,
+                            businessBenefit,
+                            businessGroup
+                    );
+                    myBusinessShop.adapter.notifyDataSetChanged();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                Log.i("서버에서 받은 전체 내용 : ", "null");
             }
         }
     }
