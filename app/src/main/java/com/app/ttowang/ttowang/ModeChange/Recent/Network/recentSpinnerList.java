fed42f6d57a42ed67cf07d1b76fed9f28ac3d86c
je0489 <je0489@naver.com>
2016-09-25 10:03:14 +0900

    recentSpinnerList Class upload

---
+++ app/src/main/java/com/app/ttowang/ttowang/ModeChange/Recent/Network/recentSpinnerList.java
@@ -0,0 +1,20 @@
+package com.app.ttowang.ttowang.ModeChange.Recent.Network;
+
+import com.app.ttowang.ttowang.ModeChange.Recent.*;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jangjieun on 2016. 9. 16..
+ */
+public class recentSpinnerList {
+    public static ArrayList<recentSpinner> spinner;
+
+    public void setSpinner(ArrayList<recentSpinner> spinner) {
+        this.spinner = spinner;
+    }
+
+    public ArrayList<recentSpinner> getSpinner() {
+        return spinner;
+    }
+}
